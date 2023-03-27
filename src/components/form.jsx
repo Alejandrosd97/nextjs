bae02e82a2e722c5c466608dce06b59e4556dc25
alejandro <alex.santamaria97@gmail.com>
2023-03-27 14:45:53 +0200

    terminado formulario

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -12,16 +12,18 @@ export default function Form() {
         <h2 className='titulo'>CONTACT</h2>
         <form className={styles.form}>
             <div className={styles.row}>
-                <span className={focus1 ? styles.labelActive : styles.labelHidden}>Name:</span>
-                <input onFocus={()=>setFocus1(true)} onBlur={()=>setFocus1(false)} placeholder='Name' type="text" required={true} id='name'/>
+                <input type="text" required={true} />
+                <span >Name</span>
+
             </div>
             <div className={styles.row}>
-                <label className={focus2 ? styles.labelActive : styles.labelHidden} htmlFor="email">Email:</label>
-                <input onFocus={()=>setFocus2(true)} onBlur={()=>setFocus2(false)} type="email" required={true} id='email'/>
+                
+                <input type="email" required={true}/>
+                <span>Email</span>
             </div>
             <div className={styles.row}>
-                <label className={focus3 ? styles.labelActive : styles.labelHidden} htmlFor="question">Query:</label>
-                <textarea className='textarea' onFocus={()=>setFocus3(true)} onBlur={()=>setFocus3(false)} required={true} id="question"></textarea>
+                <textarea required={true} id="question"></textarea>
+                <span>Message</span>
             </div>
         </form>
     </div>
